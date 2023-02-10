a7071c79d6d9010782e86a6c14007059213bbd09
Minal_Mukhi <minalmukhi06@gmail.com>
2023-02-10 19:58:42 +0530

    Assignment3

---
+++ src/test/java/com/driver/test/Main.java
@@ -0,0 +1,9 @@
+package com.driver.test;
+
+public class Main {
+    public static void main(String[] args){
+        RWOnly obj=new RWOnly();
+        obj.setName("Minal");
+        System.out.println(obj.getName());
+    }
+}
--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -4,10 +4,5 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 public class TestCases {
-    public static void main(String[] args){
-              RWOnly obj=new RWOnly();
-              obj.setName("Minal");
-              System.out.println(obj.getName());
-    }
-    
+
 }
